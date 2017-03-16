abaf5c8b03c6530c31799307fc79f1c0084f934b
MindOverflow <toxaSmith@yahoo.com>
2017-03-16 21:35:39 +0300

    [add] Добавил файл Log4NetAppenderAdapterInitializationBase.cs и RollingFileAdapterInitialization.cs в решение Logging.Log4Net. В классах содержатся автоматические свойства и их инициализация в конструкторах. Очень простые классы.

---
+++ Logging.Log4Net/Log4NetAppenderAdapterInitializationBase.cs
@@ -0,0 +1,19 @@
+﻿using Common.Logging;
+
+namespace Logging.Log4Net
+{
+    public class Log4NetAppenderAdapterInitializationBase : InitializationBase
+    {
+        public string Layout
+        {
+            get;
+            set;
+        }
+
+        public Log4NetAppenderAdapterInitializationBase()
+        {
+            // В конструкторе происходит инициализация свойства Layout
+            Layout = "%message%newline";
+        }
+    }
+}
---
+++ Logging.Log4Net/RollingFileAdapterInitialization.cs
@@ -0,0 +1,76 @@
+﻿using log4net.Appender;
+
+namespace Logging.Log4Net
+{
+    public class RollingFileAdapterInitialization : Log4NetAppenderAdapterInitializationBase
+    {
+        public string LogFileName
+        {
+            get;
+            set;
+        }
+
+        public bool AppendToFile
+        {
+            get;
+            set;
+        }
+
+        public bool ImmediateFlush
+        {
+            get;
+            set;
+        }
+
+        public int CountDirection
+        {
+            get;
+            set;
+        }
+
+        public bool StaticLogFileName
+        {
+            get;
+            set;
+        }
+
+        public RollingFileAppender.RollingMode RollingStyle
+        {
+            get;
+            set;
+        }
+
+        public int MaxSizeRollBackups
+        {
+            get;
+            set;
+        }
+
+        public string MaximumFileSize
+        {
+            get;
+            set;
+        }
+
+        public string DatePattern
+        {
+            get;
+            set;
+        }
+
+        // В какой файл писать логи.
+        public RollingFileAdapterInitialization()
+        {
+            // В конструкторе происходит инициализация всех свойств, кроме
+            // свойства MaximumFileSize.
+            LogFileName = @"${LOCALAPPDATA}\Logs\log.txt";
+            AppendToFile = true;
+            CountDirection = -1;
+            MaxSizeRollBackups = 10;
+            StaticLogFileName = true;
+            ImmediateFlush = true;
+            RollingStyle = RollingFileAppender.RollingMode.Once;
+            DatePattern = ".yyyy-MM-dd";
+        }
+    }
+}
